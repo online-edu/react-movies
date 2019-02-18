320184d6e3903bb8f28b9521ec43283c09a14692
Hardik Pithva <hardikpthv@gmail.com>
2019-02-18 23:39:25 +0530

    fix: follow eslint rules and resolve conflict

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.scss";
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+const App = () => (
+    <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+            <img src={logo} className="App-logo" alt="logo" />
         </header>
-      </div>
-    );
-  }
-}
+    </div>
+);
 
 export default App;
