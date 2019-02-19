bb21ede3f3e2c623ee27975da776edecd60ea1fc
Hardik Pithva <hardikpthv@gmail.com>
2019-02-19 21:02:07 +0530

    build: add demo to gh pages

---
+++ docs/precache-manifest.6cb6f4d79a4b341dbc81b9818ba81809.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ba707192f7081b400c0",
+    "url": "/online-edu/react-movies/static/css/main.d2b337a9.chunk.css"
+  },
+  {
+    "revision": "1ba707192f7081b400c0",
+    "url": "/online-edu/react-movies/static/js/main.1ba70719.chunk.js"
+  },
+  {
+    "revision": "47bb740a16dc6a57227f",
+    "url": "/online-edu/react-movies/static/js/runtime~main.47bb740a.js"
+  },
+  {
+    "revision": "a0a590cd59e4a4c88f0f",
+    "url": "/online-edu/react-movies/static/js/2.a0a590cd.chunk.js"
+  },
+  {
+    "revision": "f2d5d34bd16045586dfb3abfda2a0d2e",
+    "url": "/online-edu/react-movies/index.html"
+  }
+];
